91186e9ca74dabef90fca8e41bebe5de333a2963
Aditi Upadhyay <155651282+upadhyayaditi@users.noreply.github.com>
2024-02-11 10:36:07 +0530

    Add files via upload
    uploading Website

---
+++ script.js
@@ -0,0 +1,40 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('section');
+
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active');
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+//change dark theme
+
+var icon = document.querySelector('#icon');
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "images/sun.png";
+    }else{
+        icon.src = "images/moon.png"
+    }
+}
